0a28a65d7867161987609cef94996eb49e88c67c
ran <crhgc2011@hotmail.com>
2019-06-20 05:15:08 +0800

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+# Setup:
+setup(name='pbdlib',
+      version='0.1',
+      description='Programming by Demonstration module for Python',
+      url='',
+      author='Emmanuel Pignat',
+      author_email='emmanuel.pignat@idiap.ch',
+      license='MIT',
+      packages=find_packages(),
+      install_requires = ['numpy','scipy','matplotlib', 'sklearn', 'dtw', 'jupyter', 'enum', 'termcolor'],
+      zip_safe=False)
